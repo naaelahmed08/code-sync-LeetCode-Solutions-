37e41f80b10faf37432a5a67d26743c63966355d
naaelahmed08 <naaelahmed@gmail.com>
2025-09-24 07:42:58 -0400

    Update solution for maximum-subarray

--- leetcode/maximum-subarray.py
+++ leetcode/maximum-subarray.py
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // length = nums.size(); 
+        int length = nums.size(); 
         int max_sum = INT_MIN;
-        for(int start = 0; start < nums.size(); start ++){
+        for(int start = 0; start < length; start ++){
             int curr_sum = 0;
-            for(int end = start; end < nums.size(); end++){
+            for(int end = start; end < length; end++){
                 curr_sum = curr_sum + nums[end];
                 max_sum = max(max_sum , curr_sum);
                 if(curr_sum < 0) curr_sum = 0;
